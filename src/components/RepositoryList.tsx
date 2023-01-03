1fa296c3010fdd58146bf315a9a9d9969f379c52
Abraão Oliveira <abraaooliveira.eeq@gmail.com>
2023-01-03 19:45:40 -0300

    abraao2501

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -11,7 +11,7 @@ export function RepositoryList(){
   const [repositories, setRepositories] = useState<Repository[]>([]);
   
   useEffect(() => {
-    fetch('https://api.github.com/orgs/rocketseat/repos')
+    fetch('https://api.github.com/users/abraao2501/repos')
       .then(response => response.json())
       .then(data => setRepositories(data))
   }, []);
